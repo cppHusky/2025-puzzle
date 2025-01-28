6a5db3652a036bd374a40bd07410442ee724f5df
cppHusky <88606244+cppHusky@users.noreply.github.com>
2025-01-28 21:43:02 +0800

    Update cal.py

--- CatchGlowworm/cal.py
+++ CatchGlowworm/cal.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/python
 import sys
 rate=.5
 delta=.001
@@ -6,7 +6,7 @@ d0=float(sys.argv[1])
 d1=float(sys.argv[2])
 d2=float(sys.argv[3])
 d3=float(sys.argv[4])
-x=2*d3*((d0+rate*delta)**2+delta**2-d1**2)/(2*delta*(d0+rate*delta))+delta
-y=2*d3*((d1+rate*delta)**2+delta**2-d2**2)/(2*delta*(d1+rate*delta))+delta
-z=2*d3*((d2+rate*delta)**2+delta**2-d3**2)/(2*delta*(d2+rate*delta))+delta
+x=2*d3*((d0+rate*delta)**2-delta**2-d1**2)/(2*delta*d1)+delta
+y=2*d3*((d1+rate*delta)**2-delta**2-d2**2)/(2*delta*d2)+delta
+z=2*d3*((d2+rate*delta)**2-delta**2-d3**2)/(2*delta*d3)+delta
 print(f'{x} {y} {z}')
